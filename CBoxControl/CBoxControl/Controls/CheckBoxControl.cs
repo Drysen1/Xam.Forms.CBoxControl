7a0c777a2ec095e0f70fa07e4e7a9bcc54747f8c
Erik Drysén <erik.drysen@outlook.com>
2018-05-30 12:58:37 +0200

    Add project files.

---
+++ CBoxControl/CBoxControl/Controls/CheckBoxControl.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace CBoxControl.Controls
+{
+    public class CheckBoxControl : Image
+    {
+        private static string CheckboxUnCheckedImage = Device.RuntimePlatform == Device.UWP ? "Images/ic_checkbox_unchecked.png" : "ic_checkbox_unchecked.png";
+        private static string CheckboxCheckedImage = Device.RuntimePlatform == Device.UWP ? "Images/ic_checkbox_checked.png" : "ic_checkbox_checked.png";
+
+        public CheckBoxControl()
+        {
+            Source = CheckboxUnCheckedImage;
+            var imageTapGesture = new TapGestureRecognizer();
+            imageTapGesture.Tapped += ImageTapGestureOnTapped;
+            GestureRecognizers.Add(imageTapGesture);
+            PropertyChanged += OnPropertyChanged;
+        }
+
+        private void ImageTapGestureOnTapped(object sender, EventArgs eventArgs)
+        {
+            if (IsEnabled)
+            {
+                Checked = !Checked;
+            }
+        }
+
+        /// <summary>
+        /// The checked changed event.
+        /// </summary>
+        public event EventHandler<bool> CheckedChanged;
+
+        /// <summary>
+        /// The checked state property.
+        /// </summary>
+        public static readonly BindableProperty CheckedProperty = BindableProperty.Create("Checked", typeof(bool), typeof(CheckBoxControl), false, BindingMode.TwoWay, propertyChanged: OnCheckedPropertyChanged);
+
+        public bool Checked
+        {
+            get
+            {
+                return (bool)GetValue(CheckedProperty);
+            }
+
+            set
+            {
+                if (Checked != value)
+                {
+                    SetValue(CheckedProperty, value);
+                    CheckedChanged?.Invoke(this, value);
+                }
+            }
+        }
+
+        private void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e?.PropertyName == IsEnabledProperty.PropertyName)
+            {
+                Opacity = IsEnabled ? 1 : 0.5;
+            }
+        }
+
+        private static void OnCheckedPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var checkBox = bindable as CheckBoxControl;
+            if (checkBox != null)
+            {
+                var value = newValue as bool?;
+                checkBox.Checked = value.GetValueOrDefault();
+                checkBox.Source = value.GetValueOrDefault() ? CheckboxCheckedImage : CheckboxUnCheckedImage;
+            }
+        }
+    }
+}
---
+++ CBoxControl/CBoxControl/ViewModels/MainViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace CBoxControl.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private bool _rememberMe;
+        private string _mainText;
+
+        public bool RememberMe { get { return _rememberMe; } set { _rememberMe = value; OnPropertyChanged("RememberMe"); } }
+
+        public string MainText { get { return _mainText; } set { _mainText = value; OnPropertyChanged("MainText"); } }
+
+        public ICommand BtnCmd { get; set; }
+
+        public MainViewModel()
+        {
+            RememberMe = false;
+            BtnCmd = new Command(Btn_Clicked);
+        }
+
+        private void Btn_Clicked(object obj)
+        {
+            MainText = RememberMe ? "You are remembered!" : "You are not remembered :(";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName]string propertyName = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
